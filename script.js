785a072dd1157cda22a7b7be30b75dd58b901600
Stephanie Liu <sliu@rjmetrics.com>
2016-03-14 17:05:23 -0400

    create insert empty div

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('this works');
+
+var div = document.createElement('div');
+div.style.cssText = 'width: 300px; height: 300px; position: absolute; right: 0; bottom: 0; background-color: rgba(0,0,0,1);';
+document.body.appendChild(div);
