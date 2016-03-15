0d35161c729bf2393bd6d462e508f26c991b268a
Stephanie Liu <sliu@rjmetrics.com>
2016-03-15 17:20:00 -0400

    code review fixes

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 // Set 1
 
-var div;
-
 // create a buildBox function
 function buildBox() {
 	// create and insert an empty div on the page with: positioning, top and left coordinates, background color expressed as rgba 
-	div = document.createElement('div');
-	div.style.cssText = 'width: 300px; height: 300px; position: absolute; top: 0; left: 0; background-color: rgba(0,0,0,1);';
+	var div = document.createElement('div');
+	div.style.position = 'absolute';
+	setStyles(div);
 	document.body.appendChild(div);
+	return div;
 }
 
 // write a function "randInRange" that generates a random integer, given a range of possible values (min & max)
 function randInRange(min, max) {
-	return Math.floor(Math.random()*(max-min+1))+min;
+	var range = max-min;
+	return Math.floor(Math.random()*range)+min;
 }
 
 // write a function "setStyles" that accepts an element and sets all numerical CSS properties to random values within logically derived ranges.
 function setStyles(element) {
-	element.style.width = randInRange(1,600)+'px';
-	element.style.height = randInRange(1,500) + 'px';
-	element.style.left = randInRange(0,100) + 'px';
-	element.style.top = randInRange(0,100) + 'px';
-	element.style.backgroundColor = 'rgba(' + randInRange(0,256) + ',' + randInRange(0,256) + ',' + randInRange(0,256) + ',' + randInRange(0,1) + ')';
+	var windowWidth = window.innerWidth,
+		windowHeight = window.innerHeight,
+		divWidth = randInRange(1,600),
+		divHeight = randInRange(1,500),
+		leftMax = windowWidth - divWidth,
+		topMax = windowHeight - divHeight;
+
+	element.style.width = divWidth + 'px';
+	element.style.height = divHeight + 'px';
+	element.style.left = randInRange(0,leftMax) + 'px';
+	element.style.top = randInRange(0,topMax) + 'px';
+	element.style.backgroundColor = 'rgba(' + randInRange(0,256) + ',' + randInRange(0,256) + ',' + randInRange(0,256) + ',' + Math.random() + ')';
 }
+
+buildBox();
